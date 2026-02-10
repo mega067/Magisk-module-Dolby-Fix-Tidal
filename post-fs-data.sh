b64543a463c19e1599e612fc0c747c0fa2901cc4
mega067 <angeljuares067@gmail.com>
2026-02-09 22:49:35 -0600

    feat: Add Android 15 compatibility by including new libraries and updating permissions, and update module version.

--- post-fs-data.sh
+++ post-fs-data.sh
@@ -6,7 +6,16 @@ MODDIR=${0%/*}
 # Log start
 echo "Dolby Fix Module: post-fs-data started" > /cache/magisk_dolby_fix.log
 
-# Ensure permissions (just in case)
+# Ensure permissions
+chmod 755 $MODDIR/system/vendor/bin/hw/vendor.dolby.hardware.dms@2.0-service
 chmod 644 $MODDIR/system/vendor/etc/media_codecs_dolby_audio.xml
+chmod 644 $MODDIR/system/vendor/lib/hw/sound_trigger.primary.msmnile.so
+chmod 644 $MODDIR/system/odm/lib/liba2dpoffload.so
+chmod 644 $MODDIR/system/odm/lib/libaudioEngineerTest.so
+chmod 644 $MODDIR/system/vendor/lib/libhdmipassthru.so
+chmod 644 $MODDIR/system/vendor/lib/libssrec.so
+chmod 644 $MODDIR/system/vendor/lib64/vendor.dolby.hardware.dms@2.0-impl.so
+chmod 644 $MODDIR/system/vendor/lib64/vendor.dolby.hardware.dms@2.0.so
+chmod 644 $MODDIR/system/vendor/etc/init/vendor.dolby.hardware.dms@2.0-service.rc
 
-echo "Dolby Fix Module: permissions set" >> /cache/magisk_dolby_fix.log
+echo "Dolby Fix Module: permissions set for Android 15 binaries" >> /cache/magisk_dolby_fix.log
